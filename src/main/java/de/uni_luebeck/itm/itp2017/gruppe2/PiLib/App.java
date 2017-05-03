56e7348dce66dc96211a114d6fa5317bf6275765
Daniel Rickert <daniel.rickert@student.uni-luebeck.de>
2017-05-03 08:31:37 +0200

    Removed Start-Arguments

--- src/main/java/de/uni_luebeck/itm/itp2017/gruppe2/PiLib/App.java
+++ src/main/java/de/uni_luebeck/itm/itp2017/gruppe2/PiLib/App.java
@@ -11,9 +11,10 @@ import de.uni_luebeck.itm.itp2017.gruppe2.PiLib.tasks.Task1_3;
 public class App {
 	private static final ITask[] tasks = new ITask[]{new Task1_2(), new Task1_3()};
 	public static void main(String[] args) throws Throwable {
-		if(args.length > 0) {
-			tasks[Integer.parseInt(args[0])].run(args);
-		}
+		new Task1_3().run(args);
+//		if(args.length > 0) {
+//			tasks[Integer.parseInt(args[0])].run(args);
+//		}
 	}
 
 }
